fcb6240633f0e3ee0639eec9cc5c3688fd3b670d
konate00 <okonate0913@gmail.com>
2023-10-17 11:59:49 -0400

    End of the first lecture

---
+++ Demo1.js
@@ -0,0 +1,15 @@
+"use strict"; // To be Always use on top of every jsCode 
+
+var firstName; // a variable is the place to put a thing
+
+firstName = "Ousmane"; // litteral 'string'. String is the word for text
+                       // firstName = "Ousmane";
+                       // var bookTitle;
+                       // bookTitle = "Ousmane's advice on life";
+                       // bookTitle = 'Ousmane is writing this book "on life\"';
+
+console.log(firstName); 
+
+console.log("Ousmane");
+
+
---
+++ Demo2.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var firstName;
+var lastName;
+
+// assign a value "Matt" & "Konate" to the variable labeled firstName and lastName
+firstName = "Ousmane";
+lastName = "Konate";
+
+var fullName;
+
+//Assigned to the variable fullName the result of the expression in ln 13. 
+fullName = lastName + ", " + firstName;
+
+var message;
+
+message = "The user's full name is: " + fullName;
+
+console.log(message);
+
+console.log(lastName);
+
+console.log(fullName);
+
+
